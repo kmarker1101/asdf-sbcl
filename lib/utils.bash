be48a63e4807a0f5a3b115a6b99d65b0ebb688d3
Kevin <kmarker@gmail.com>
2023-12-14 13:25:33 -0600

    update release path

--- lib/utils.bash
+++ lib/utils.bash
@@ -42,7 +42,7 @@ download_release() {
 	filename="$2"
 
 	# TODO: Adapt the release URL convention for sbcl
-	url="$GH_REPO/releases/v${version}.tar.gz"
+	url="$GH_REPO/releases/${version}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
